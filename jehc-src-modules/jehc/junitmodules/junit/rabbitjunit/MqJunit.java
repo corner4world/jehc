24eb1d4c8868e4a84af98091da0804a5d45c5cb2
dengcj <244831954@qq.com>
2017-07-20 13:19:23 +0800

    submit

--- jehc-src-modules/jehc/junitmodules/junit/rabbitjunit/MqJunit.java
+++ jehc-src-modules/jehc/junitmodules/junit/rabbitjunit/MqJunit.java
@@ -2,16 +2,18 @@ package jehc.junitmodules.junit.rabbitjunit;
 
 import org.junit.Test;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import jehc.xtmodules.xtcore.base.BaseJunit;
 import jehc.xtmodules.xtcore.util.springutil.GetApplicationContext;
 public class MqJunit extends BaseJunit{
+	@Autowired
+	AmqpTemplate rabbitTemplate;
 	@Test
 	public void rabbit(){
 //		ApplicationContext  ctx = new ClassPathXmlApplicationContext("classpath*:/jehc/xtmodules/xtcore/sources/mq/rabbit.xml");
-		AmqpTemplate rabbitTemplate=(AmqpTemplate)GetApplicationContext.getBean("rabbitTemplate");
 		rabbitTemplate.convertAndSend("queue1k", "生产者发送一条紧急通知任务");
 	}
 }
--- jehc-src-modules/jehc/junitmodules/junit/xtjunit/Xt_UserinfoJunit.java
+++ jehc-src-modules/jehc/junitmodules/junit/xtjunit/Xt_UserinfoJunit.java
@@ -1,4 +1,7 @@
 package jehc.junitmodules.junit.xtjunit;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,25 +9,27 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import jehc.xtmodules.xtcore.base.BaseJunit;
+import jehc.xtmodules.xtcore.util.springutil.GetApplicationContext;
 import jehc.xtmodules.xtmodel.Xt_Userinfo;
 import jehc.xtmodules.xtservice.Xt_UserinfoService;
 
 public class Xt_UserinfoJunit extends BaseJunit{
-	//模拟request,response  
-    private MockHttpServletRequest request;  
-    private MockHttpServletResponse response;   
-    //执行测试方法之前初始化模拟request,response  
-    @Before    
-    public void setUp(){    
-        request = new MockHttpServletRequest();      
-        request.setCharacterEncoding("UTF-8");      
-        response = new MockHttpServletResponse();      
-    } 
+//	//模拟request,response  
+//    private MockHttpServletRequest request;  
+//    private MockHttpServletResponse response;   
+//    //执行测试方法之前初始化模拟request,response  
+//    @Before    
+//    public void setUp(){    
+//        request = new MockHttpServletRequest();      
+//        request.setCharacterEncoding("UTF-8");      
+//        response = new MockHttpServletResponse();      
+//    } 
 	@Autowired
 	private Xt_UserinfoService xt_UserinfoService;
 	@Test
 	public void getXtUserinfoById(){
-		Xt_Userinfo xt_Userinfo = xt_UserinfoService.getXtUserinfoById("00f377cd266c4fc38e1575a559bd842d");
-		System.out.println(xt_Userinfo.getXt_departinfo_name());
+//		Xt_UserinfoService service =(Xt_UserinfoService)GetApplicationContext.getBean("xt_UserinfoService");
+		Map<String, Object> condition = new HashMap<String, Object>();
+		System.out.println(xt_UserinfoService.getXtUserinfoList(condition).size());
 	}
 }
