88a523f663fb2b27d6dafcb17591fe890614bbd2
dengcj <244831954@qq.com>
2017-09-12 16:20:57 +0800

    滚动条去除

--- WebRoot/view/pc/xt-view/xt-login-logs/xt-login-logs-detail.js
+++ WebRoot/view/pc/xt-view/xt-login-logs/xt-login-logs-detail.js
@@ -51,6 +51,11 @@ function initXtLoginLogsFormDetail(){
 			readOnly:true,
 			margin:'2 5 4 5'
 		},
+		/**新方法使用开始**/  
+        scrollable:true,  
+        scrollable:'x',
+        scrollable:'y',
+        /**新方法使用结束**/ 
 		items:[
 		{
 			fieldLabel:'日志编号',
