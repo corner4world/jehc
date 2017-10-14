70f41888a7d8210c9c248773a9ee035ffc6fb27e
dengcj <244831954@qq.com>
2017-10-14 09:39:45 +0800

    删除目录WebRoot

--- WebRoot/deng/editor/kindeditor/plugins/hello/hello.js
+++
@@ -1,6 +0,0 @@
-KindEditor.plugin('hello', function(K) {
-        var editor = this, name = 'hello';
-        editor.clickToolbar(name, function() {
-                editor.insertHtml('���');
-        });
-});
